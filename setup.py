f884c902054f281dd166501ea1a892420ce9abfc
Thomasina Lee <thomasina.lee@gmail.com>
2016-01-27 14:15:08 +0000

    create dummy package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='testing16',
+      version='0.1',
+      description=('We copy the funniest joke in the world '
+                   'and reduce its functionality'),
+      url='http://github.com/thomasina-lee/testing16',
+      author='Noname',
+      author_email='noname@neverexist',
+      license='MIT',
+      packages=['testing16'],
+      zip_safe=False)
